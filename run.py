1a9d7a91f421965e7322974fbca052f2cfefe004
Hiroki KAWASAKI <73154404+kwsk-h@users.noreply.github.com>
2022-05-30 15:06:50 +0900

    sample作成
    helloに対して返信するだけのBot
    トークン情報は非公開，token.jsonは.gitignoreしてある

---
+++ run.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 29 17:16:04 2022
+
+@author: kwsk0
+"""
+
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import json
+
+#jsonからトークンを読み取る
+with open('token.json') as f:
+    jsn = json.load(f)
+
+
+# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+# app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=jsn["SLACK_BOT_TOKEN"]["token"])
+
+# 'hello' を含むメッセージをリッスンします
+# 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
+# https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"Hey there <@{message['user']}>!")
+
+# アプリを起動します
+if __name__ == "__main__":
+    # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    SocketModeHandler(app, jsn["SLACK_APP_TOKEN"]["token"]).start()
