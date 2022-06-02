71b5c1e349f9160b14746148948c4dd7d00de63c
tommyham <tob19098.kez052080uns@gmail.com>
2022-06-03 00:39:48 +0900

    役割分担を送信する関数
    仕事と担当者の二次元配列から分担表のメッセージを作成し，Slackに送信する関数の作成

--- roll_chart.py
+++ roll_chart.py
@@ -49,23 +49,59 @@ def message_hello(message, say):
         text=f"Hey there <@{message['user']}>!"  # 表示されなくなる
     )
 
+# 仕事場所と担当者の二次元配列からメッセージを作成する関数
+def makeComent(array):
+    blocks=[]
+    # 仕事一つ当たりのblockのテンプレート※うまくいかなかった
+    template={
+                "type": "section",
+                "text": {
+                "type": "plain_text",
+                "text": None,
+                "emoji": True}
+            }
+    
+    text=""
+    # 始めはBlockを生成して，それを送ろうとしたが，dictのリストがblocksの引数にとれない
+    # 単一のblockとし，textに改行文字を入れて一つのtxtでメッセージを作成
+    for a in array:
+        # template["text"]=a[0]+" "+a[1]
+        # blocks.append(template.copy())
+        text+=a[0]+" "+a[1]+"\n"
+    template["text"]=text
+    blocks=template
+    return blocks
+
+# 分担表をSlackに送信する関数
 @app.message("分担表")
 def send_roll_chart(message,say):
+    array=[["ゴミ捨て1","川崎"],["ゴミ捨て2","富濱"],["ゴミ捨て3","布野"],["教員室1","増尾"],["教員室2","松前"]]
+    
+    # 当初は単一のblockを渡そうとしたがblockの引数にとれなかった
+    # 生成したblockの中から表示したいtxtのみ抽出し，それをblocksのtextのtextに代入
+    block=makeComent(array)
+    tex=block["text"]
+    
     # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
     say(
         blocks=[
             {
-			"type": "header",
-			"text": {
-				"type": "plain_text",
-				"text": "New request",
-				"emoji": True
+                "type": "section",
+                "text": {
+                    "type":"plain_text",
+                    "text":tex,
+                    "emoji":True
+                    }
             }
-            }
-            ],        
-            text=f"Hey there <@{message['user']}>!"  # 表示されなくなる
-                )
+            ],
+        text=f"Hey there <@{message['user']}>!"  # 表示されなくなる
+        )
+
+
 # アプリを起動します
 if __name__ == "__main__":
     # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
     SocketModeHandler(app, jsn["SLACK_APP_TOKEN"]["token"]).start()
+
+# array=[["ゴミ捨て1","川崎"],["ゴミ捨て2","富濱"],["ゴミ捨て3","布野"],["教員室1","増尾"],["教員室2","松前"]]
+# blocks=makeBlockComent(array)
