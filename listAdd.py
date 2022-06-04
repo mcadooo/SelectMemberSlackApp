5db4eeade390b6eaf393207d766101f486e99037
tommyham <tomihama-keigo-kc@ynu.jp>
2022-06-04 18:40:47 +0900

    しごと追加を実行するボタン
    更新，確認するボタンを追加

--- listAdd.py
+++ listAdd.py
@@ -5,6 +5,7 @@ Created on Fri Jun  3 23:50:07 2022
 @author: lonytom
 """
 
+from email import message
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -16,6 +17,16 @@ with open('token.json') as f:
 
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 app = App(token=jsn["SLACK_BOT_TOKEN"]["token"])
+
+# 仕事一覧の辞書型を改行入りの一つの文字列に変換する関数
+def makeWorkComment(work_list):
+    text=""
+    for w in work_list:
+        print(w)
+        w=work_list[w]
+        text+=w["name"]+"  "+w["weight"]+"  "+w["need"]+"\n"
+    
+    return text
 
 # 与えられた配列から選択肢を作成する関数
 # num:0=仕事，1＝メンバ
@@ -57,6 +68,8 @@ def selectAddWork(message,say):
     need=makeWorkWeightOptions(1, 20)
 	
     # イベントがトリガーされたチャンネルへ say() で複数選択肢を送信します
+
+    # 追加する仕事名の入力フォーム送信
     say(
         blocks=[
             {
@@ -80,6 +93,7 @@ def selectAddWork(message,say):
         text=f"Hey there <@{message['user']}>!"  # 表示されなくなる
         )
 	
+    # 追加する仕事の重みを入力
     say(
             blocks=[
     		{
@@ -99,6 +113,7 @@ def selectAddWork(message,say):
             text=f"Hey there <@{message['user']}>!"  # 表示されなくなる
             )
     
+    # 追加する仕事の必要人数を入力
     say(
             blocks=[
     		{
@@ -118,7 +133,43 @@ def selectAddWork(message,say):
             text=f"Hey there <@{message['user']}>!"  # 表示されなくなる
             )
     
-    path="./log/temp_work.json"
+    # 追加した仕事のリストを確認，更新を実行するボタン
+    say(
+        blocks=[
+            {
+                "type": "actions",
+                "elements":[
+                    {
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "確認",
+						"emoji": True
+					},
+					"value": "click_check_work",
+					"action_id": "check_work_list"
+				},
+                {
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "更新",
+						"emoji": True
+					},
+					"value": "click_update_work",
+					"action_id": "update_work_list"
+				},
+                ]
+            }
+        ]
+
+    )
+    
+    base_path="./log/"
+    if not os.path.isdir(base_path):
+        os.makedirs(base_path)
+    
+    path=base_path+"temp_work.json"
     work_list=dict()
     
     # jsonファイルへの書き出しに差し替え
@@ -182,7 +233,51 @@ def selectAddWork(message,say):
         # jsonファイルへの書き出しに差し替え
         with open(path, mode='w') as file:
             json.dump(work_list, file, ensure_ascii=False, indent=2)
+    
+    @app.action("check_work_list")
+    def checkWorkList(body,ack,say):
+        ack()
+        path="./log/temp_work.json" # 当日の仕事リストのパス(場所とファイル名はしらないので要変更)
+        # jsonファイルへの読み込みに差し替え
+        with open(path, mode='r') as file:
+            all_work_today = json.load(file)
+        
+        text=makeWorkComment(all_work_today)
+        print(text)
+        
+        # チャンネルに選択したそうじを投稿(確認用)
+        say(
+        	blocks=[
+        	{
+        		"type": "section",
+        		"text": {
+        			"type": "plain_text",
+        			"text": text,
+        			"emoji": True
+        		}
+        	}
+        ]
+        )
 
+@app.action("update_work_list")
+def updateWorkList(message,say):
+    path_work_list="./log/work_copy.json" # 当日の仕事リストのパス(場所とファイル名はしらないので要変更)
+    path_temp_work="./log/temp_work.json" # 一次的に追加したい仕事のリストのパス
+    work_list=dict()
+    
+    # jsonファイルへの読み込みに差し替え
+    with open(path_work_list, mode='r') as file:
+        work_list = json.load(file)
+    
+    # jsonファイルへの読み込みに差し替え
+    with open(path_temp_work, mode='r') as file:
+        temp_work = json.load(file)
+    
+    work_list["work"+str(len(work_list))]=temp_work
+
+    # jsonファイルへの書き出しに差し替え
+    with open(path_work_list, mode='w') as file:
+        json.dump(work_list, file, ensure_ascii=False, indent=2)
 
 # アプリを起動します
 if __name__ == "__main__":
